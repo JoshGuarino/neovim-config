06f4754611cabafac0e5d9b935eb1601e1cf8879
Josh Guarino <jguarino722@gmail.com>
2024-11-07 12:03:17 -0500

    update config

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,12 +6,12 @@
 local M = {}
 
 M.base46 = {
-  theme = "gruvbox",
+  theme = "catppuccin",
 
-  -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- 	["@comment"] = { italic = true },
-  -- },
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
 }
 
 M.nvdash = {
